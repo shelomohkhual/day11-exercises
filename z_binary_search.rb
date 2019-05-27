6e256b690d8754db8298c45c5cadcc0d3b82210a
Shelo <YOUR@EMAIL.com>
2019-05-27 17:05:17 +0800

    sync to OSX

--- z_binary_search.rb
+++ z_binary_search.rb
@@ -2,8 +2,26 @@
 # Write a method, binary search(array, target), that accepts an array and another input as target
 # The method should use the binary search algorithm to return the position of the element where the target is found
 
-def binary_search(array, target)
-	#Your code here
+#pseudocode
+# check the length of array and divide by two
+# check if the target is larger or smaller, if smaller divide the lenght into 2 again
+# and continue loop
+
+# if smm
+
+
+def binary_search(target, array)
+	start_index = 0
+	last_index = array.length
+	mid = (start_index + last_index) / 2
+
+	while mid <= target	do
+		elsif mid >= target
+			mid = mid *2
+		end
+	return mid
+	puts mid(index)
+end
 end
 
 test_array = (100..200).to_a
--- z_linear_search.rb
+++ z_linear_search.rb
@@ -1,12 +1,37 @@
 # In Class Exercise: Linear Search
 # Write a method, linear_search(array, target), that accepts an array and another input as target
-# The method should use the binary search algorithm to return the position of the element where the target is found
+# The method should use the liner search algorithm to return the position of the element where the target is found
 # You may not use any Array or Enum methods except length or size
 # You may not use any Enum derived iterations, only for, while or until
 
-def linear_search(target, searchfrom)
-	#Your code here
+# pseudocode
+
+
+def linear_search(target, array)
+	# 	i = 0
+	# 	while i < array.length
+	# 	  if array[i] == target
+	# 		return i
+	# 	  end
+	# 	  i += 1
+	# 	end
+	# 	 "Not found"
+	#   end
+
+	for target in array do
+		if array == target
+			return array.index(array)
+		end
+	end
+	return "not found"
 end
+
+
+	# until target ==  |random_numbers|
+	# 	random_numbers.inject(target)
+	# 	if target == random_numbers
+	# 		puts random_numbers[:target]
+
 
 random_numbers = [6,29,18,2,72,19,18,10,37]
 p linear_search(18, random_numbers)
